1daf979b271499a774aee09b521ad425cc969ba4
benhaderle <benhaderle@gmail.com>
2017-02-21 15:47:45 -0500

    Added pace text + calculator

--- Assets/Scripts/BodyController.cs
+++ Assets/Scripts/BodyController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BodyController : MonoBehaviour {
 
@@ -8,11 +9,18 @@ public class BodyController : MonoBehaviour {
     GameObject rightLeg;
     Vector3 destination;
 
+    float paceTimer = 2;
+    float pace = 480;
+    Vector3 lastPacePos;
+    Text paceText;
+
 	// Use this for initialization
 	void Start () {
         leftLeg = GameObject.Find("Left Leg");
         rightLeg = GameObject.Find("Right Leg");
+        paceText = GameObject.Find("Pace Text").GetComponent<Text>();
         destination = transform.position;
+        lastPacePos = transform.position;
 	}
 	
 	// Update is called once per frame
@@ -27,5 +35,24 @@ public class BodyController : MonoBehaviour {
         if (destination.z - transform.position.z < .1f)
             transform.position = destination;
         transform.position = Vector3.Lerp(transform.position, destination, .2f);
+
+        paceTimer -= Time.deltaTime;
+        if (paceTimer < 0) {
+            float paceDifference = transform.position.z - lastPacePos.z;
+            pace = 540f - (paceDifference * 4f);
+
+            int paceMinutes = (int)(pace / 60);
+            int paceSeconds = (int)(pace % 60);
+            if (paceMinutes > 8)
+                paceText.text = "Pace: C'MON\nPR: 6:40";
+            else {
+                if (paceSeconds < 10)
+                    paceText.text = "Pace: " + paceMinutes + ":0" + paceSeconds + "\nPR: 6:40";
+                else
+                    paceText.text = "Pace: " + paceMinutes + ":" + paceSeconds + "\nPR: 6:40";
+            }
+            paceTimer = 2;
+            lastPacePos = transform.position;
+        }
 	}
 }
