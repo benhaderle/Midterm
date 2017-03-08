9eb3bc1082de8a1cfdd58cfc27dbe9b9d964acbd
benhaderle <benhaderle@gmail.com>
2017-03-08 01:50:41 -0500

    Made an arm, s[tarted some code with it

--- Assets/Scripts/LegController.cs
+++ Assets/Scripts/LegController.cs
@@ -9,13 +9,17 @@ public class LegController : MonoBehaviour {
     public float distanceToTravel;
     public GameObject otherLeg;
     public GameObject keyText;
+    public GameObject arm;
 
     public Vector3 destination;
+    public float armDest;
+
 
     // Use this for initialization
     void Start () {
         destination = transform.position;
-	}
+        armDest = arm.transform.rotation.eulerAngles.x;
+    }
 
     // Update is called once per frame
     void Update() {
@@ -24,11 +28,33 @@ public class LegController : MonoBehaviour {
             if (Input.GetKeyDown(key) && transform.localPosition.z < distanceToTravel / 2 && transform.position.z >= destination.z - 1) {
                 keyText.SetActive(false);
                 destination = transform.position + (transform.forward * distanceToTravel * 2);
+
+                if (armDest == 310f) {
+                    armDest = 100f;
+                    otherLeg.GetComponent<LegController>().armDest = 310;
+                }
+                else if (armDest == 100f) {
+                    armDest = 310f;
+                    otherLeg.GetComponent<LegController>().armDest = 100;
+                }
+                else {
+                    armDest = 100f;
+                    otherLeg.GetComponent<LegController>().armDest = 310;
+                }
             }
-            if (Mathf.Abs(transform.position.z - destination.z) < .5f)
-                transform.position = destination;
+                if (Mathf.Abs(transform.position.z - destination.z) < .5f)
+                    transform.position = destination;
+                else
+                    transform.position = Vector3.Lerp(transform.position, destination, .45f);
+            }
+
+         //   Debug.Log(armDest);
+        Debug.Log(otherLeg.GetComponent<LegController>().armDest);
+
+            if (Mathf.Abs(armDest - arm.transform.eulerAngles.x) < 30)
+                arm.transform.eulerAngles = new Vector3(armDest, arm.transform.eulerAngles.y, arm.transform.eulerAngles.z);
             else
-                transform.position = Vector3.Lerp(transform.position, destination, .45f);
+                arm.transform.eulerAngles = new Vector3(Mathf.LerpAngle(arm.transform.eulerAngles.x, armDest, .45f), arm.transform.eulerAngles.y, arm.transform.eulerAngles.z);
         }
-    }
+    
 }
